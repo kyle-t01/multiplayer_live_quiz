e905763aa8287468df01ffa5a7744e4bc43e3909
kylet-01 <userEmail>
2025-04-08 11:40:10 +1000

    first commit

---
+++ backend/src/test/kotlin/com/example/multiplayerquizgame/MultiplayerQuizGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.multiplayerquizgame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiplayerQuizGameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
