a683bc447824c763bc18d09c15792a0664b1df83
kylet-01 <userEmail>
2025-04-09 14:43:00 +1000

    add skeleton for GameController class

---
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/controller/GameController.kt
@@ -0,0 +1,11 @@
+package com.example.multiplayerquizgame.controller
+
+import com.example.multiplayerquizgame.model.*
+import org.springframework.web.socket.WebSocketSession
+
+class GameController (private val lobby: Lobby) {
+    // handle player attempting to join lobby
+
+
+
+}
