516aba729459117ab354569afd3025cc87a280c5
kylet-01 <userEmail>
2025-04-09 15:53:55 +1000

    added helper functions for Lobby to abstract code

--- backend/src/main/kotlin/com/example/multiplayerquizgame/model/Lobby.kt
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/model/Lobby.kt
@@ -8,4 +8,38 @@ class Lobby(
     val players: MutableMap<WebSocketSession, Player> = mutableMapOf(),
     val quiz: Quiz = Quiz(),
     var isGameStarted: Boolean = false,
-)
+
+) {
+
+    // remove a <session, player> from lobby on disconnect
+    fun removePlayer(session: WebSocketSession) {
+        val player= players[session]
+        println("$player LEFT the game!")
+        players.remove(session)
+
+        // if there are no more players, then the game has ended
+        if (players.isEmpty()) {
+            isGameStarted = false;
+        }
+        return
+    }
+
+    // return a List<Player> currently in the lobby
+    fun getPlayers():List<Player> {
+        return players.values.toList()
+    }
+
+    // add a <session, player> pair to players
+    fun addToPlayers(session: WebSocketSession, player: Player) {
+        // associate the session with this player
+        players[session] = player
+        return
+    }
+
+    // start the game
+    fun startGame() {
+        println("Game has officially started.")
+        isGameStarted = true
+    }
+
+}
