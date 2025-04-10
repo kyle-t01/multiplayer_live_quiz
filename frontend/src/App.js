b87afa4bd6e623b02946c972e4aa1472fee88c66
kylet-01 <userEmail>
2025-04-10 12:19:52 +1000

    updated main app title and description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,11 @@ function App() {
 
 	return (
 		<div className="app">
-			<h1>Welcome to the APP DEMO</h1>
+			<h1>Welcome to Mutiplayer Live Quiz!</h1>
+			<p>It is a project built with Kotlin + SpringBoot WebSockets</p>
 			<Lobby />
 			<InputBox />
 			<QuestionDisplay />
-
 		</div>
 	);
 }
