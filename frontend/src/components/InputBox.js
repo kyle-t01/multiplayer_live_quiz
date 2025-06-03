ca60c1c8001017cdd2d13d34e0c8bc6d102bbded
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-06-03 12:41:34 +1000

    players may now input roomCode to join a room

--- frontend/src/components/InputBox.js
+++ frontend/src/components/InputBox.js
@@ -4,10 +4,10 @@ import { GlobalVars } from "../context/GlobalContext";
 const InputBox = () => {
 
     // global state
-    const { playerName, setPlayerName, handlePlayerJoin, hasJoined } = GlobalVars();
+    const { playerName, setPlayerName, roomCode, setRoomCode, handlePlayerJoin, hasJoined } = GlobalVars();
 
     const maxChars = 10;
-
+    const maxCodesize = 4;
 
     return (
         <div className="input-box" hidden={hasJoined}>
@@ -15,7 +15,7 @@ const InputBox = () => {
             <input
                 className="input"
                 type="text"
-                placeholder="Username..."
+                placeholder="USERNAME..."
                 value={playerName}
                 onChange={(e) => { setPlayerName(e.target.value) }}
                 rows={1}
@@ -24,8 +24,17 @@ const InputBox = () => {
             <div className="char-counter">
                 {playerName.length} / {maxChars}
             </div>
+            <input
+                className="input"
+                type="text"
+                placeholder="ROOM CODE..."
+                value={roomCode}
+                onChange={(e) => { setRoomCode(e.target.value) }}
+                rows={1}
+                maxLength={maxCodesize}
+            />
             <button className="button" onClick={handlePlayerJoin}>
-                Join Lobby
+                Connect to Room
             </button>
         </div>
     );
