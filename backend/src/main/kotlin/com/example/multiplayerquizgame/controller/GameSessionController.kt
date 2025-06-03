3a5b3b5a01a32da0879652ffa21f9519a2996240
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-06-03 17:39:03 +1000

    handle game events traffic based on type

--- backend/src/main/kotlin/com/example/multiplayerquizgame/controller/GameSessionController.kt
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/controller/GameSessionController.kt
@@ -4,6 +4,7 @@ import com.example.multiplayerquizgame.model.Game
 import com.example.multiplayerquizgame.model.GameEvent
 import com.example.multiplayerquizgame.model.GameEventType
 import com.example.multiplayerquizgame.model.Lobby
+import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.web.socket.WebSocketSession
 
 class GameSessionController (private val lobby: Lobby, private val emitter: Emitter) {
@@ -14,46 +15,74 @@ class GameSessionController (private val lobby: Lobby, private val emitter: Emit
     // manage a list of games
     val games: MutableList<GameLoopController> = mutableListOf()
 
-    fun createGame() {
+    fun createGame(): GameLoopController? {
         if (games.size >= MAX_GAMES) {
             // EXCEED max games
             println("Too many games, currently ${games.size}")
+            return null
         }
         // else, add the game
         val game = GameLoopController(lobby, emitter)
         games.add(game)
-        return
+        println("game ${game.getRoomCode()} created")
+        return game
     }
 
     private fun findGameRoomFromSession(session: WebSocketSession): GameLoopController? {
         val roomCode = lobby.getRoomCodeFromSession(session) ?: return null
+        return findGameRoomFromRoomCode(roomCode)
+    }
+
+    private fun findGameRoomFromRoomCode(roomCode: String?): GameLoopController? {
+        if (roomCode == null || roomCode == "") return null
         return games.find {it.getRoomCode() == roomCode}
     }
 
     fun handleGameEventTraffic(session: WebSocketSession, gameEvent: GameEvent) {
-        // are we creating a new room?
+        // when creating or joining a game, not possible to associate session with game
         val type = gameEvent.type
-        if (type == GameEventType.CREATE) {
-            // yes, so create new room
-            createGame()
+        val jsonData = gameEvent.data as JsonNode
+        var game: GameLoopController? = null
+        when(type) {
+            GameEventType.CREATE -> {
+                // create game
+                game = createGame()
+            }
+            GameEventType.JOIN -> {
+                // find game by roomCode
+                val roomCode = jsonData.get("roomCode")?.asText()
+                game = findGameRoomFromRoomCode(roomCode)
+                // can't find game, then KICK
+                if (game == null) {
+                    println("Room code $roomCode did not match any games!")
+                    emitter.emit(session, GameEvent(GameEventType.KICK, ""))
+                }
+            }
+            else -> {
+                // find game by session
+                game = findGameRoomFromSession(session)
+            }
         }
-
-        // find roomCode associated with this session
-        val game = findGameRoomFromSession(session)
+        // could we find the game?
         if (game == null) {
-            println("could not associate session with game!")
+            // no, just return
             return
         }
-        // otherwise, redirect game event to the game
+        // otherwise, handle game event
         game.handleGameEvent(session, gameEvent)
         return
 
     }
 
     fun handleConnectionClosed(session: WebSocketSession) {
+        // find game associated with session
         val game = findGameRoomFromSession(session)
-        require(game != null) {"There MUST be a game where session is disc. from!"}
-        // delegate to game loop
+        // is associated with a game?
+        if (game == null) {
+            // no, so do nothing
+            return
+        }
+        // yes, then delegate disconnect to game loop
         game.handleDisconnect(session)
         // if there are no players in the game, can safely remove it
         if (game.hasNoPlayers()) {
