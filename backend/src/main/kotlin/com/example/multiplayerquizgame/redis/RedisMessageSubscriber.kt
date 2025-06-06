3e49b2774a7d1b2fbfc4838e6c595ca796325c6a
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-06-06 18:58:27 +1000

    delegate messages to game session controller

--- backend/src/main/kotlin/com/example/multiplayerquizgame/redis/RedisMessageSubscriber.kt
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/redis/RedisMessageSubscriber.kt
@@ -2,6 +2,7 @@ package com.example.multiplayerquizgame.redis
 
 import com.example.multiplayerquizgame.controller.GameSessionController
 import com.example.multiplayerquizgame.util.JsonMapper
+import com.fasterxml.jackson.databind.JsonNode
 import org.springframework.data.redis.connection.Message
 import org.springframework.data.redis.connection.MessageListener
 import org.springframework.stereotype.Component
@@ -23,11 +24,39 @@ class RedisMessageSubscriber(
      */
     override fun onMessage(message: Message, pattern: ByteArray?) {
         try {
-            val json = message.body.decodeToString()
-            println("this was the message recieved: $json")
-            println("doing something with game session controller")
+            // ie: game-room:1234
+            val topic = message.channel.decodeToString()
+            // ie: raw data
+            val bodyMessage = message.body.decodeToString()
+            println("this was the message received: [$topic]:$bodyMessage")
+            // extract topic
+            val topicParts = getTopicParts(topic)
+            val prefix = topicParts[0]
+            val id = topicParts[1]
+            // handle body
+            val gameEvent = mapper.convertStrToGameEvent(bodyMessage)
+            println("gameEvent: $gameEvent")
+            // handle message
+            when (prefix) {
+                "game-room" -> gameSessionController.handleRedisRoomEvent(id,gameEvent)
+                "player-id" -> gameSessionController.handleRedisPlayerEvent(id, gameEvent)
+                else -> println("unknown $prefix")
+            }
+
+
+
         }catch (e: Exception) {
             println("Error: ${e.message}")
         }
     }
+
+    private fun getTopicParts(topic: String): List<String> {
+        val parts = topic.split(":")
+        if (parts.size != 2) {
+            println("topic format invalid $topic")
+            return listOf("","")
+        }
+        return parts
+    }
+
 }
