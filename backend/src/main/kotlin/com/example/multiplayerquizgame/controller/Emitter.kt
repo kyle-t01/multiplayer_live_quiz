b9b04935d65c61f123919c0a5dc65df6ca755164
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-06-07 10:44:46 +1000

    emit to local player via websocket session, else use redis pub sub for external players

--- backend/src/main/kotlin/com/example/multiplayerquizgame/controller/Emitter.kt
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/controller/Emitter.kt
@@ -2,12 +2,19 @@ package com.example.multiplayerquizgame.controller
 
 import com.example.multiplayerquizgame.model.GameEvent
 import com.example.multiplayerquizgame.model.GameEventType
+import com.example.multiplayerquizgame.model.Lobby
+import com.example.multiplayerquizgame.model.Player
+import com.example.multiplayerquizgame.redis.RedisMessagePublisher
 import com.example.multiplayerquizgame.util.JsonMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.WebSocketSession
 
 @Component
-class Emitter(private val mapper: JsonMapper) {
+class Emitter(private val mapper: JsonMapper,
+              private val lobby: Lobby,
+              private val publisher: RedisMessagePublisher
+)
+{
     // send data to sessions
     /**
      * Emit signal to a player
@@ -30,6 +37,41 @@ class Emitter(private val mapper: JsonMapper) {
         for (s in sessionList){
             emit(s, event)
         }
+    }
+
+    /**
+     * Emit to all players
+     *
+     * Emit to local websocket session, else use redis pub/sub
+     *
+     * @param players
+     * @param event
+     */
+    fun emitToAllPlayers(players: List<Player>, event: GameEvent){
+        for (p in players) {
+            val s = lobby.getSessionFromPlayer(p)
+            if (s != null) {
+                emit(s, event)
+                continue
+            }
+            // can not find session, must be external player
+            println("session not found, using redis pub/sub")
+            //publisher.publish(event)
+        }
+
+    }
+
+    // emit to a player (whether local or external)
+    fun emitToPlayer(player: Player, event: GameEvent) {
+        val session = lobby.getSessionFromPlayer(player)
+        if (session != null) {
+            // handle local player via websocket session
+            emit(session, event)
+            return
+        }
+        // handle external player via redis pub/sub
+        publisher.publishToPlayer(player.getID(), event)
+
     }
 
 }
