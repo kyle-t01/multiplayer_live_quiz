46332a3866a5fe23c7029c3e540ad949dc3c5562
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-06-07 10:45:06 +1000

    player now has a UUID

--- backend/src/main/kotlin/com/example/multiplayerquizgame/model/Player.kt
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/model/Player.kt
@@ -1,12 +1,17 @@
 package com.example.multiplayerquizgame.model
+import java.util.UUID
+
 
 data class Player(
     val name: String,
     var qCorrect: Int = 0,
     var roomCode: String
 ) {
+    private val id: String = UUID.randomUUID().toString()
+
     fun resetScore() {
         qCorrect = 0
     }
-    
+
+    fun getID() = id
 }
