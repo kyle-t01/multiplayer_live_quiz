badc7faf9b14c20a1b8d0af7bf6c56033b6774f8
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-07-04 12:25:45 +1000

    Add Logger object

---
+++ backend/src/main/kotlin/com/example/multiplayerquizgame/log/Logger.kt
@@ -0,0 +1,43 @@
+package com.example.multiplayerquizgame.log
+import org.springframework.stereotype.Component
+
+// for now, Logger will be used as a singleton class accessible by everyone
+@Component
+object Logger {
+    // There are three types of logs
+    // (1) Websocket logs - websocket GameEvents sent to backend gameLoop by player
+    // (2) Backend Pod logs - redis, pod init logs, inter-pod communications
+    // (3) GameLoop logs - logs created during game loop, game creation and deletion
+    // simple format: [severity] [podName] [roomCode] [timeStamp] [details]
+    private var podName:String? = getPodName()
+
+
+    fun logWS(roomCode: String?, details: String?) {
+        formattedLog("ws", podName, roomCode, getCurrentTime(), details)
+    }
+
+    fun logGL(roomCode: String?, details: String?) {
+        formattedLog("gl", podName, roomCode, getCurrentTime(), details)
+    }
+
+    fun logPod(roomCode: String?, details: String?) {
+        formattedLog("pod", podName, roomCode, getCurrentTime(), details)
+    }
+
+    fun getPodName(): String {
+        if (podName == null) {
+            podName = System.getenv("HOSTNAME") ?: ""
+        }
+        return podName!!
+    }
+
+    private fun getCurrentTime(): String {
+        return java.time.LocalDateTime.now().toString()
+    }
+
+    private fun formattedLog(category: String, podName: String?, roomCode: String?, timestamp: String?, details: String?) {
+        println("$category, $podName, $roomCode, $timestamp, $details")
+        // save to database
+    }
+
+}
