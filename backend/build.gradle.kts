3cca0210141fa1e8c113bd67955558876b34e9d9
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-07-04 18:02:39 +1000

    add postgresql

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.postgresql:postgresql")
+
 }
 
 kotlin {
